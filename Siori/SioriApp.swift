02a1bef2a9d171cf2170ef01c2de6d8adb3fac53
onigiri-z <k.tennis218@icloud.com>
2021-11-19 15:46:01 +0900

    Initial Commit

---
+++ Siori/SioriApp.swift
@@ -0,0 +1,17 @@
+//
+//  SioriApp.swift
+//  Siori
+//
+//  Created by 入江健太 on 2021/11/19.
+//
+
+import SwiftUI
+
+@main
+struct SioriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
